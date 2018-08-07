f763bc4c96576f5097987f1d84df4660e418bd32
SubhamAshish <33892535+SubhamAshish@users.noreply.github.com>
2018-08-07 15:14:42 +0530

    Update StartupProducer.java

--- DemoSpringCloudStreamProducer/src/main/java/com/demo/StartupProducer.java
+++ DemoSpringCloudStreamProducer/src/main/java/com/demo/StartupProducer.java
@@ -1,22 +1,32 @@
 package com.demo;
 
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.transaction.RabbitTransactionManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @EnableBinding(EmailChannel.class)
-public class StartupProducer extends SpringBootServletInitializer{
+@EnableTransactionManagement
+public class StartupProducer extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(StartupProducer.class, args);
 	}
-	
+
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
 		return application.sources(StartupProducer.class);
+	}
+
+	@Bean
+	public RabbitTransactionManager transactionManager(ConnectionFactory cf) {
+		return new RabbitTransactionManager(cf);
 	}
 
 }
